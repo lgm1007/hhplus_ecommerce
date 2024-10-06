0f7d0015df458c8bc670bc59a291932bd03f1218
lgm1007 <flxl654@gmail.com>
2024-10-06 19:17:37 +0900

    feat: 프로젝트 생성

---
+++ src/main/kotlin/com/example/hhplus_ecommerce/HhplusEcommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hhplus_ecommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HhplusEcommerceApplication
+
+fun main(args: Array<String>) {
+	runApplication<HhplusEcommerceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/hhplus_ecommerce/HhplusEcommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.hhplus_ecommerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HhplusEcommerceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
