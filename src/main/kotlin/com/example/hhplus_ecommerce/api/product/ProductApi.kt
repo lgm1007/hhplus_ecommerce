9c8e4e2d5cca64f32b4a28d2e4034f6a5649e28a
lgm1007 <flxl654@gmail.com>
2024-10-08 20:43:36 +0900

    feat: Mock API - 상품 조회 API의 Mock API 추가

---
+++ src/main/kotlin/com/example/hhplus_ecommerce/api/product/ProductApi.kt
@@ -0,0 +1,21 @@
+package com.example.hhplus_ecommerce.api.product
+
+import com.example.hhplus_ecommerce.api.product.response.ProductResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/api/v1")
+@RestController
+class ProductApi() {
+	@GetMapping("/products")
+	fun getProducts(): ResponseEntity<List<ProductResponse>> {
+		return ResponseEntity.ok(
+			listOf(
+				ProductResponse(1L, "상품 A", 5000, 10),
+				ProductResponse(2L, "상품 B", 3000, 5),
+			)
+		)
+	}
+}
