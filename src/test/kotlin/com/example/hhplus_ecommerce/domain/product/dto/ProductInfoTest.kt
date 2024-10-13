4e0c382cb48da3cdef6caa9517a8aa96c1d20082
lgm1007 <flxl654@gmail.com>
2024-10-13 10:18:22 +0900

    test: ProductInfo의 정적 팩토리 메서드 단위 테스트 추가

---
+++ src/test/kotlin/com/example/hhplus_ecommerce/domain/product/dto/ProductInfoTest.kt
@@ -0,0 +1,64 @@
+package com.example.hhplus_ecommerce.domain.product.dto
+
+import com.example.hhplus_ecommerce.domain.product.ProductCategory
+import org.assertj.core.api.AssertionsForClassTypes.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+class ProductInfoTest {
+	@Test
+	@DisplayName("ProductDto와 ProductDetailDto 객체로 ProductInfo를 생성하는 정적 팩토리 메서드 기능 테스트")
+	fun productInfoOf() {
+		val productDto = ProductDto(
+			1L,
+			"상품A",
+			10000,
+			"상품A 설명",
+			LocalDateTime.now(),
+		)
+
+		val productDetailDto = ProductDetailDto(
+			1L,
+			1L,
+			100,
+			ProductCategory.CLOTHES,
+			LocalDateTime.now(),
+			LocalDateTime.now(),
+		)
+
+		val actual = ProductInfo.of(productDto, productDetailDto)
+
+		assertThat(actual).isNotNull
+		assertThat(actual.productId).isEqualTo(1L)
+		assertThat(actual.name).isEqualTo("상품A")
+		assertThat(actual.price).isEqualTo(10000)
+		assertThat(actual.stockQuantity).isEqualTo(100)
+	}
+	
+	@Test
+	@DisplayName("ProductDto 목록과 ProductDetailDto 목록으로 ProductInfo 목록을 생성하는 정적 팩토리 메서드 기능 테스트")
+	fun productInfoListOf() {
+		val productDtos = listOf(
+			ProductDto(1L, "상품A", 10000, "상품A 설명", LocalDateTime.now()),
+			ProductDto(2L, "상품B", 5000, "상품B 설명", LocalDateTime.now()),
+		)
+		val productDetailDtos = listOf(
+			ProductDetailDto(1L, 1L, 100, ProductCategory.CLOTHES, LocalDateTime.now(), LocalDateTime.now()),
+			ProductDetailDto(2L, 2L, 30, ProductCategory.COSMETICS, LocalDateTime.now(), LocalDateTime.now()),
+		)
+
+		val actual = ProductInfo.listOf(productDtos, productDetailDtos)
+
+		assertThat(actual.size).isEqualTo(2)
+		assertThat(actual[0].productId).isEqualTo(1L)
+		assertThat(actual[0].name).isEqualTo("상품A")
+		assertThat(actual[0].price).isEqualTo(10000)
+		assertThat(actual[0].stockQuantity).isEqualTo(100)
+		assertThat(actual[1].productId).isEqualTo(2L)
+		assertThat(actual[1].name).isEqualTo("상품B")
+		assertThat(actual[1].price).isEqualTo(5000)
+		assertThat(actual[1].stockQuantity).isEqualTo(30)
+	}
+}
