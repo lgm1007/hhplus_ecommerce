ed8ab98dc3a83d8586b0cfa8323c3c0f31ffe78c
lgm1007 <flxl654@gmail.com>
2024-10-14 22:10:33 +0900

    feat: 비용 Service에서 userId로 해당 사용자의 비용 정보 조회하는 로직 구현

---
+++ src/main/kotlin/com/example/hhplus_ecommerce/domain/balance/BalanceService.kt
@@ -0,0 +1,14 @@
+package com.example.hhplus_ecommerce.domain.balance
+
+import com.example.hhplus_ecommerce.domain.balance.dto.BalanceDto
+import org.springframework.stereotype.Service
+
+@Service
+class BalanceService(
+	private val balanceRepository: BalanceRepository,
+	private val balanceHistoryRepository: BalanceHistoryRepository
+) {
+	fun getByUserId(userId: Long): BalanceDto {
+		return balanceRepository.getByUserId(userId)
+	}
+}
