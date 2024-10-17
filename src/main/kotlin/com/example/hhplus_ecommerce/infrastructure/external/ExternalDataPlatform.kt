6f4544abc081abc06e0eb1ed27bfaa21a7d9c9ce
lgm1007 <flxl654@gmail.com>
2024-10-18 00:01:14 +0900

    modify: 외부 플랫폼 요청 중 예외 발생했다고 가정

--- src/main/kotlin/com/example/hhplus_ecommerce/infrastructure/external/ExternalDataPlatform.kt
+++ src/main/kotlin/com/example/hhplus_ecommerce/infrastructure/external/ExternalDataPlatform.kt
@@ -6,10 +6,6 @@ import com.example.hhplus_ecommerce.exception.ExternalRequestException
 class ExternalDataPlatform {
 	fun sendPaymentData(paymentResultInfo: PaymentResultInfo) {
 		// 외부 플랫폼 요청 중 예외 발생했다고 가정
-		try {
-			throw ExternalRequestException("Error:: 외부 데이터 플랫폼에 전송 중 예외 발생")
-		} catch (e: Exception) {
-			println(e.message)
-		}
+		throw ExternalRequestException("Error:: 외부 데이터 플랫폼에 전송 중 예외 발생")
 	}
 }
