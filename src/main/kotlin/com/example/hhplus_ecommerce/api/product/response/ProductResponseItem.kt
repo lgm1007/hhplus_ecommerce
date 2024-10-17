7f78e15c55568b83570cdb1bd4efbe243dc48baa
lgm1007 <flxl654@gmail.com>
2024-10-17 20:00:28 +0900

    modify: 상품 Response에 정적 팩토리 메서드 추가

--- src/main/kotlin/com/example/hhplus_ecommerce/api/product/response/ProductResponseItem.kt
+++ src/main/kotlin/com/example/hhplus_ecommerce/api/product/response/ProductResponseItem.kt
@@ -1,9 +1,25 @@
 package com.example.hhplus_ecommerce.api.product.response
 
+import com.example.hhplus_ecommerce.domain.product.dto.ProductInfo
+
 class ProductResponseItem(
 	val id: Long,
 	val name: String,
 	val price: Int,
 	val stockQuantity: Int
 ) {
+	companion object {
+		fun from(productInfo: ProductInfo): ProductResponseItem {
+			return ProductResponseItem(
+				productInfo.productId,
+				productInfo.name,
+				productInfo.price,
+				productInfo.stockQuantity
+			)
+		}
+
+		fun fromList(productInfos: List<ProductInfo>): List<ProductResponseItem> {
+			return productInfos.map(::from)
+		}
+	}
 }
