26786e4f27d2ebe612cceacf0b6c871ac32ef530
lgm1007 <flxl654@gmail.com>
2024-10-17 22:58:04 +0900

    test: 결제 Facade 기능에 대한 통합테스트 작성

--- src/main/kotlin/com/example/hhplus_ecommerce/infrastructure/external/ExternalDataPlatform.kt
+++ src/main/kotlin/com/example/hhplus_ecommerce/infrastructure/external/ExternalDataPlatform.kt
@@ -1,10 +1,15 @@
 package com.example.hhplus_ecommerce.infrastructure.external
 
 import com.example.hhplus_ecommerce.domain.payment.dto.PaymentResultInfo
+import com.example.hhplus_ecommerce.exception.ExternalRequestException
 
 class ExternalDataPlatform {
 	fun sendPaymentData(paymentResultInfo: PaymentResultInfo) {
-		println("외부 데이터 플랫폼에 전송 성공. " +
-			"[paymentId = ${paymentResultInfo.paymentId}, orderId = ${paymentResultInfo.orderId}, 결제 일자 = ${paymentResultInfo.paymentDate}]")
+		// 외부 플랫폼 요청 중 예외 발생했다고 가정
+		try {
+			throw ExternalRequestException("Error:: 외부 데이터 플랫폼에 전송 중 예외 발생")
+		} catch (e: Exception) {
+			println(e.message)
+		}
 	}
 }
