8e542572841ba831db9ea7cebab256fd7ff8a931
lgm1007 <flxl654@gmail.com>
2024-10-22 20:55:28 +0900

    feat: GlobalExceptionHandler 예외에 대한 에러 로그 추가

--- src/main/kotlin/com/example/hhplus_ecommerce/api/advice/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/hhplus_ecommerce/api/advice/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.example.hhplus_ecommerce.api.advice
 import com.example.hhplus_ecommerce.api.error.ErrorBody
 import com.example.hhplus_ecommerce.exception.BadRequestException
 import com.example.hhplus_ecommerce.exception.NotFoundException
+import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+	private val logger = KotlinLogging.logger {}
+
 	/**
 	 * BadRequest 예외 처리
 	 * code: 400
@@ -17,6 +20,7 @@ class GlobalExceptionHandler {
 	 */
 	@ExceptionHandler(BadRequestException::class)
 	fun handleBadRequestException(e: BadRequestException): ResponseEntity<ErrorBody> {
+		logger.error(e.errorStatus.message, e)
 		return ResponseEntity(ErrorBody(e.errorStatus.message, 400), HttpStatus.BAD_REQUEST)
 	}
 
@@ -27,6 +31,7 @@ class GlobalExceptionHandler {
 	 */
 	@ExceptionHandler(NotFoundException::class)
 	fun handleNotFoundException(e: NotFoundException): ResponseEntity<ErrorBody> {
+		logger.error(e.errorStatus.message, e)
 		return ResponseEntity(ErrorBody(e.errorStatus.message, 404), HttpStatus.NOT_FOUND)
 	}
 }
