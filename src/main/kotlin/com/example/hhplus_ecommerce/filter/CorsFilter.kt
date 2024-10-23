0c378a7065cca6d15637d4f25fc1bf2036214a07
lgm1007 <flxl654@gmail.com>
2024-10-23 22:03:44 +0900

    feat: CorsFilter 추가 - FE 연동한다는 가정하에 CORS 에러 대비하는 필터 추가

---
+++ src/main/kotlin/com/example/hhplus_ecommerce/filter/CorsFilter.kt
@@ -0,0 +1,30 @@
+package com.example.hhplus_ecommerce.filter
+
+import org.springframework.stereotype.Component
+import javax.servlet.Filter
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class CorsFilter : Filter {
+	override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+		val httpRequest = request as HttpServletRequest
+		val httpResponse = response as HttpServletResponse
+
+		httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+		httpResponse.setHeader("Access-Control-Allow-Credentials", "true")
+		httpResponse.setHeader("Access-Control-Allow-Methods","*")
+		httpResponse.setHeader("Access-Control-Max-Age", "3600")
+		httpResponse.setHeader("Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+		if ("OPTIONS" == httpRequest.method) {
+			httpResponse.status = HttpServletResponse.SC_OK
+		} else {
+			chain?.doFilter(request, response)
+		}
+	}
+}
