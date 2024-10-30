3b716ea2256670900c513b383e12b563d10eaf5d
lgm1007 <flxl654@gmail.com>
2024-10-30 23:41:53 +0900

    test: spring-kafka-test 기능으로 테스트 환경에서 Kafka 기능 사용하도록 통합 테스트 작성

--- src/test/kotlin/com/example/hhplus_ecommerce/usecase/order/OrderFacadeIntegrationTest.kt
+++ src/test/kotlin/com/example/hhplus_ecommerce/usecase/order/OrderFacadeIntegrationTest.kt
@@ -4,12 +4,15 @@ import com.example.hhplus_ecommerce.domain.order.dto.OrderItemInfo
 import com.example.hhplus_ecommerce.domain.product.ProductCategory
 import com.example.hhplus_ecommerce.domain.product.ProductService
 import com.example.hhplus_ecommerce.exception.BadRequestException
+import com.example.hhplus_ecommerce.infrastructure.message.consumer.KafkaConsumer
+import com.example.hhplus_ecommerce.infrastructure.message.producer.MessageProducer
 import com.example.hhplus_ecommerce.infrastructure.order.OrderItemJpaRepository
 import com.example.hhplus_ecommerce.infrastructure.order.OrderJpaRepository
 import com.example.hhplus_ecommerce.infrastructure.product.ProductDetailJpaRepository
 import com.example.hhplus_ecommerce.infrastructure.product.ProductJpaRepository
 import com.example.hhplus_ecommerce.infrastructure.product.entity.Product
 import com.example.hhplus_ecommerce.infrastructure.product.entity.ProductDetail
+import com.fasterxml.jackson.databind.ObjectMapper
 import mu.KotlinLogging
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -17,11 +20,13 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.test.context.EmbeddedKafka
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 3, brokerProperties = ["listeners=PLAINTEXT://localhost:9092"], ports = [9092])
 class OrderFacadeIntegrationTest {
 	@Autowired private lateinit var orderFacade: OrderFacade
 	@Autowired private lateinit var productService: ProductService
@@ -29,6 +34,9 @@ class OrderFacadeIntegrationTest {
 	@Autowired private lateinit var productDetailRepository: ProductDetailJpaRepository
 	@Autowired private lateinit var orderRepository: OrderJpaRepository
 	@Autowired private lateinit var orderItemRepository: OrderItemJpaRepository
+	@Autowired private lateinit var messageProducer: MessageProducer
+	@Autowired private lateinit var kafkaConsumer: KafkaConsumer
+	@Autowired private lateinit var objectMapper: ObjectMapper
 	private val logger = KotlinLogging.logger {}
 
 	@BeforeEach
@@ -193,6 +201,44 @@ class OrderFacadeIntegrationTest {
 			assertThat(actual.stockQuantity).isEqualTo(0)
 			assertThat(successCount.get()).isEqualTo(20)
 			assertThat(failCount.get()).isEqualTo(10)
+		} finally {
+			executor.shutdown()
+		}
+	}
+
+	@Test
+	@DisplayName("주문에 대한 동시 주문 요청 Kafka 이벤트 발행 기능 테스트")
+	fun productOrderConcurrencyWithKafka() {
+		val productId = productRepository.save(Product("상품 A", "A 상품")).id
+		val detailId = productDetailRepository.save(ProductDetail(productId, 1000, 20, ProductCategory.CLOTHES)).id
+
+		val executor = Executors.newFixedThreadPool(30)
+		val countDownLatch = CountDownLatch(30)
+
+		try {
+			val startTime = System.currentTimeMillis()
+			repeat(30) {
+				executor.submit {
+					try {
+						orderFacade.productOrderWithKafka(1L, listOf(OrderItemInfo(detailId, 1)))
+					} catch (e: Exception) {
+						logger.info("예외 발생!")
+					} finally {
+						countDownLatch.countDown()
+					}
+				}
+			}
+
+			countDownLatch.await()
+
+			val endTime = System.currentTimeMillis()
+			logger.info("실행 시간: ${endTime - startTime} milliseconds")
+
+			Thread.sleep(2000)
+
+			val actual = productService.getProductInfoById(productId)
+
+			assertThat(actual.stockQuantity).isEqualTo(0)
 		} finally {
 			executor.shutdown()
 		}
