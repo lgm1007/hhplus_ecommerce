fe06b0474eb70afa7f4c20d37cf95a9bdccebcaa
lgm1007 <flxl654@gmail.com>
2024-11-13 21:22:54 +0900

    feat: Kafka로 결제 완료 후 외부 데이터 플랫폼에 전송하는 이벤트 발행하기

--- src/main/kotlin/com/example/hhplus_ecommerce/infrastructure/external/ExternalDataPlatform.kt
+++ src/main/kotlin/com/example/hhplus_ecommerce/infrastructure/external/ExternalDataPlatform.kt
@@ -1,10 +1,10 @@
 package com.example.hhplus_ecommerce.infrastructure.external
 
-import com.example.hhplus_ecommerce.domain.payment.dto.PaymentResultInfo
 import com.example.hhplus_ecommerce.exception.ExternalRequestException
+import java.time.LocalDateTime
 
 class ExternalDataPlatform {
-	fun sendPaymentData(paymentResultInfo: PaymentResultInfo) {
+	fun sendPaymentData(orderId: Long, currentBalance: Int, paymentDate: LocalDateTime) {
 		// 외부 플랫폼 요청 중 예외 발생했다고 가정
 		throw ExternalRequestException("Error:: 외부 데이터 플랫폼에 전송 중 예외 발생")
 	}
--- src/main/kotlin/com/example/hhplus_ecommerce/infrastructure/message/producer/KafkaProducer.kt
+++ src/main/kotlin/com/example/hhplus_ecommerce/infrastructure/message/producer/KafkaProducer.kt
@@ -1,6 +1,8 @@
 package com.example.hhplus_ecommerce.infrastructure.message.producer
 
+import com.example.hhplus_ecommerce.infrastructure.message.producer.dto.PaymentDataMessage
 import com.example.hhplus_ecommerce.infrastructure.message.producer.dto.ProductMessage
+import com.example.hhplus_ecommerce.infrastructure.message.topic.DATA_PLATFORM_TOPIC
 import com.example.hhplus_ecommerce.infrastructure.message.topic.PRODUCT_ORDER_TOPIC
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
@@ -11,5 +13,9 @@ class KafkaProducer(
 ) : MessageProducer {
 	override fun sendProductOrderMessage(message: ProductMessage) {
 		kafkaTemplate.send(PRODUCT_ORDER_TOPIC, message.productDetailId.toString(), message)
+	}
+
+	override fun sendPaymentDataPlatformMessage(message: PaymentDataMessage) {
+		kafkaTemplate.send(DATA_PLATFORM_TOPIC, message.paymentId.toString(), message)
 	}
 }
